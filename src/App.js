78258a397a6090ad09ae10241bb55fde10eab2f9
philica94 <107121941+philica94@users.noreply.github.com>
2023-01-19 20:13:42 +0100

    chore: Adding bootstrap, redux, router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { Route, Switch } from 'react-router-dom';
+
+import GreenGrocery from './components/green-grocery/GreenGrocery';
+import LandingPage from './components/landing-page/LandingPage';
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. test
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Switch>
+        <Route path='/' exact>
+          <LandingPage />
+        </Route>
+        <Route path='/green-grocery'>
+          <GreenGrocery />
+        </Route>
+        <Route path='*'>
+          <LandingPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
