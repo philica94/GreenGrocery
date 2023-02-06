edc6625a38e036e3f0cf7153f8ab8b21d7d07de0
philica94 <107121941+philica94@users.noreply.github.com>
2023-02-06 14:24:44 +0100

    feature: update cart functionality

--- src/components/green-grocery/layout/CartHeader.js
+++ src/components/green-grocery/layout/CartHeader.js
@@ -1,17 +1,18 @@
 import { useSelector } from 'react-redux';
 
-import CartIcon from '../../cart/CartIcon';
+import { Cart3 } from 'react-bootstrap-icons';
+import { selectTotalCartItemsAmount } from '../../../store';
 
 const CartHeader = ({ className, openModal }) => {
-  const cart = useSelector((state) => state.cart);
+  const totalItemsAmount = useSelector(selectTotalCartItemsAmount);
 
   return (
     <div className={className} onClick={openModal}>
       <div className='pe-1'>Cart</div>
-      <div style={{ width: '2rem', height: '2rem' }}>
-        <CartIcon />
+      <div style={{ transform: 'translate(0,-10%)' }}>
+        <Cart3 />
       </div>
-      <span className='ps-1'>{cart.totalItemsAmount}</span>
+      <span className='ps-1'>{totalItemsAmount}</span>
     </div>
   );
 };
--- src/store/index.js
+++ src/store/index.js
@@ -1,36 +1,49 @@
-import { createSlice, configureStore, current } from '@reduxjs/toolkit';
+import { createSlice, configureStore, current, createSelector } from '@reduxjs/toolkit';
 
 const initialCartState = {
   items: [],
-  totalItemsAmount: 0,
 };
 
 const cartSlice = createSlice({
   name: 'updatingCart',
   initialState: initialCartState,
   reducers: {
-    addCartItem(state, action) {
-      const existingItemCartIndex = state.items.findIndex((item) => item.id === action.payload.id);
-
-      if (existingItemCartIndex !== -1) {
-        state.items[existingItemCartIndex].amount++;
-      } else {
-        state.items = [...state.items, action.payload];
-      }
-      state.totalItemsAmount += action.payload.amount;
+    increaseCartItemAmount(state, action) {
+      cartSlice.caseReducers.setItemQuantity(state, { payload: { item: action.payload, changeAmount: 1 } });
+    },
+    reduceCartItemAmount(state, action) {
+      cartSlice.caseReducers.setItemQuantity(state, { payload: { item: action.payload, changeAmount: -1 } });
     },
     removeCartItem(state, action) {
-      const existingItemCartIndex = state.items.findIndex((item) => item.id === action.payload.id);
+      cartSlice.caseReducers.setItemQuantity(state, { payload: { item: action.payload, amount: 0 } });
+    },
+    setItemQuantity(state, action) {
+      const {
+        payload: { changeAmount, amount, item },
+      } = action;
 
-      if (state.items[existingItemCartIndex].amount === 1) {
-        state.items.splice(existingItemCartIndex, 1);
+      const existingItemCartIndex = state.items.findIndex(({ id }) => item.id === id);
+      const oldAmount = state.items[existingItemCartIndex]?.amount ?? 0;
+      const newAmount = amount ?? oldAmount + changeAmount;
+
+      if (newAmount <= 0) {
+        existingItemCartIndex !== -1 && state.items.splice(existingItemCartIndex, 1);
+      } else if (!state.items[existingItemCartIndex]) {
+        state.items.push({ ...item, amount: newAmount });
       } else {
-        state.items[existingItemCartIndex].amount--;
+        state.items[existingItemCartIndex].amount = newAmount;
       }
-      state.totalItemsAmount--;
     },
   },
 });
+
+export const selectTotalCartItemsAmount = createSelector(
+  (state) => state.cart.items,
+  (items) => items.reduce((sum, { amount }) => sum + amount, 0)
+);
+
+// export const selectTotalCartItemsAmount = (passedStore) =>
+//   passedStore.cart.items.reduce((sum, { amount }) => sum + amount, 0);
 
 const store = configureStore({
   reducer: { cart: cartSlice.reducer },
