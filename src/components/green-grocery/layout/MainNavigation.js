c0213d95389b5d8a6217226b50db8533d1b856d9
philica94 <107121941+philica94@users.noreply.github.com>
2023-02-16 18:43:50 +0100

    feat: improve navbar - display back to shop button outside of shop

--- src/components/green-grocery/layout/MainNavigation.js
+++ src/components/green-grocery/layout/MainNavigation.js
@@ -1,4 +1,4 @@
-import { Link, Route } from 'react-router-dom';
+import { Link, Route, useLocation, useRouteMatch } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 import Cart from '../homepage/cart/Cart';
@@ -8,22 +8,21 @@ import LogoutButton from '../login/LogoutButton';
 
 const MainNavigation = () => {
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  const { pathname } = useLocation();
+  const greenGroceryPathMatch = useRouteMatch(['/green-grocery']);
+  const isInCheckout = pathname.includes('/green-grocery/checkout');
+  const isInTheShop = greenGroceryPathMatch?.isExact;
 
   return (
-    <nav className='navbar sticky-top bg-white mb-2 shadow-sm '>
-      <div className='container'>
-        <Route path='/green-grocery'>
-          <Link to='/' className='navbar-brand'>
-            {'<-'} Landing Page
-          </Link>
-          <Cart />
-          {!isLoggedIn && <LoginButton />}
-          {isLoggedIn && <LogoutButton />}
-        </Route>
-        <Route path='/green-grocery/checkout'>
-          <BackToShopButton />
-        </Route>
-      </div>
+    <nav className='navbar sticky-top bg-white mb-2 shadow-sm d-flex px-3'>
+      <Route path='/green-grocery'>
+        <Link to='/' className='navbar-brand'>
+          {'<-'} Landing Page
+        </Link>
+        {!isInCheckout && <Cart />}
+        {isLoggedIn ? <LogoutButton /> : <LoginButton />}
+        {!isInTheShop && <BackToShopButton />}
+      </Route>
     </nav>
   );
 };
