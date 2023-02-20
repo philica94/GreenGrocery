c7c00097799a3744c29c8d3daf0c04edeb75310c
philica94 <107121941+philica94@users.noreply.github.com>
2023-02-20 22:43:57 +0100

    feat: add favourite product functionality

--- src/components/green-grocery/homepage/products/Product.js
+++ src/components/green-grocery/homepage/products/Product.js
@@ -6,10 +6,22 @@ import ProductForm from './ProductForm';
 import ProductInfo from './ProductInfo';
 
 const Product = ({ id, name, price, image }) => {
+  const [showFavouriteStar, setShowFavouriteStar] = useState(false);
+
+  const mouseHoverHandler = () => {
+    console.log('Show');
+    setShowFavouriteStar(true);
+  };
+
+  const mouseLeaveHandler = () => {
+    console.log('Hide');
+    setShowFavouriteStar(false);
+  };
+
   return (
     <div className='col-xxl-3 col-xl-4 col-md-6 col-12'>
-      <Card>
-        <ProductInfo name={name} price={price} image={image} />
+      <Card onMouseEnter={mouseHoverHandler} onMouseLeave={mouseLeaveHandler}>
+        <ProductInfo name={name} price={price} image={image} onShowStar={showFavouriteStar} />
         <ProductForm id={id} name={name} price={price} image={image} />
       </Card>
     </div>
--- src/components/green-grocery/homepage/products/ProductInfo.js
+++ src/components/green-grocery/homepage/products/ProductInfo.js
@@ -1,6 +1,15 @@
-const ProductInfo = ({ name, price, image }) => {
+import classes from './ProductInfo.module.css';
+
+import { Star } from 'react-bootstrap-icons';
+
+const ProductInfo = ({ name, price, image, onShowStar }) => {
   return (
     <div>
+      {onShowStar && (
+        <span className={classes.starPosition}>
+          <Star size={25} />
+        </span>
+      )}
       <h6 className='m-3'>
         <img src={image} height='150px' />
       </h6>
