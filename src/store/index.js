e08d58865a81754e61e1d6fbe53656f11d447534
philica94 <107121941+philica94@users.noreply.github.com>
2023-02-23 21:53:27 +0100

    feat: Save store state in the local storage

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,25 @@ import { authSlice } from './slices/auth';
 import { cartSlice } from './slices/cart';
 import { favouriteSlice } from './slices/favourites';
 
+const localStorageMiddleware = ({ getState }) => {
+  return (next) => (action) => {
+    const result = next(action);
+    localStorage.setItem('applicationState', JSON.stringify(getState()));
+    return result;
+  };
+};
+
+const reHydrateStore = () => {
+  // console.log(JSON.parse(localStorage.getItem('applicationState')));
+  if (localStorage.getItem('applicationState') !== null) {
+    return JSON.parse(localStorage.getItem('applicationState')); // re-hydrate the store
+  }
+};
+
 const store = configureStore({
   reducer: { cart: cartSlice.reducer, auth: authSlice.reducer, favourite: favouriteSlice.reducer },
+  preloadedState: reHydrateStore(),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(localStorageMiddleware),
 });
 
 export default store;
