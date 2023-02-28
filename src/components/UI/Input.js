45287293e8509ae540397f4ee00e3e782730288e
philica94 <107121941+philica94@users.noreply.github.com>
2023-02-28 22:08:23 +0100

    feat: create SignUp component

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,12 +1,14 @@
-const Input = ({ id, labelText, wrapperClassName = 'mb-3', ...attributes }) => {
+import React from 'react';
+
+const Input = React.forwardRef(({ id, labelText, wrapperClassName = 'mb-3', ...attributes }, ref) => {
   return (
     <div className={wrapperClassName}>
       <label htmlFor={id} className='form-label'>
         {labelText}
       </label>
-      <input type={attributes.type || 'text'} className='form-control' id={id} {...attributes} />
+      <input type={attributes.type || 'text'} className='form-control' id={id} {...attributes} ref={ref} />
     </div>
   );
-};
+});
 
 export default Input;
---
+++ src/components/green-grocery/login/SignUp.js
@@ -0,0 +1,47 @@
+import { useRef } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { authActions } from '../../../store/slices/auth';
+import Button from '../../UI/Button';
+import Card from '../../UI/Card';
+import Input from '../../UI/Input';
+
+const SignUp = () => {
+  const usernameRef = useRef();
+  const passwordRef = useRef();
+  const repeatedPasswordRef = useRef();
+  const disptach = useDispatch();
+
+  const submitRegisterHandler = (e) => {
+    e.preventDefault();
+    const enteredUsername = usernameRef.current.value;
+    const enteredPassword = passwordRef.current.value;
+    const enteredRepeatedPassword = repeatedPasswordRef.current.value;
+    const userSingUpData = { username: enteredUsername, password: enteredPassword };
+
+    if (enteredPassword === enteredRepeatedPassword) {
+      disptach(authActions.signUp(userSingUpData));
+    }
+  };
+
+  return (
+    <div className='row justify-content-md-center'>
+      <Card additionalClasses='col-md-10'>
+        <h5 className='m-3'>Sign up</h5>
+        <hr />
+        <form>
+          <Input id='loginEmail' labelText='E-mail' type='email' ref={usernameRef}></Input>
+          <Input id='loginPassword' labelText='Password' type='password' ref={passwordRef}></Input>
+          <Input id='loginPasswordRepeat' labelText='Repeat password' type='password' ref={repeatedPasswordRef}></Input>
+          <div className='d-flex justify-content-end'>
+            <Button type='submit' onClick={submitRegisterHandler}>
+              Sign up
+            </Button>
+          </div>
+        </form>
+      </Card>
+    </div>
+  );
+};
+
+export default SignUp;
