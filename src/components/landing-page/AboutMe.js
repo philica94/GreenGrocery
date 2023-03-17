860776484262c37ac5c9cccb2ed12784c8cc99aa
philica94 <107121941+philica94@users.noreply.github.com>
2023-03-17 20:53:42 +0100

    feat: add new AboutMe page

--- src/components/landing-page/AboutMe.js
+++ src/components/landing-page/AboutMe.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import classes from './AboutMe.module.css';
 
 const AboutMe = () => {
   return (
-    <React.Fragment>
-      <header className='sticky-top'>
-        <span className='fs-1 fw-bold fst-italic text-primary'>Sara Wieszałka</span>
-      </header>
-      <div className='container'>
-        <p className='text-white-75 mb-5'>
-          Hello! My name is Sara Wieszałka and I am currently looking for a job as a Junior React Frontend Developer.
-          Below you can find my recent project.
-        </p>
-        <Link to='/green-grocery' className='btn btn-primary btn-xl'>
-          GreenGrocery
+    <div className={classes.main}>
+      <div className={classes.photoWrapper}>
+        <img src='./images/Sara.jpeg' />
+        <div className={classes.photoCover}></div>
+        <div className={classes.photoMargin}></div>
+      </div>
+      <div className='d-flex align-items-center flex-column justify-content-center h-100'>
+        <div className={classes.typewriter}>
+          <p>Hello! My name is Sara Wieszałka.</p>
+          <p>I am looking for a job as a Junior Front-end Developer.</p>
+          <p>Please check out the application that I created.</p>
+        </div>
+
+        <Link to='/green-grocery' className={`btn btn-primary btn-lg ${classes.link}`}>
+          Go to the application
         </Link>
       </div>
-    </React.Fragment>
+    </div>
   );
 };
 
