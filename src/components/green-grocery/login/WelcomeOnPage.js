a470959b146a497d8b6cda282779b95d87862fd8
philica94 <107121941+philica94@users.noreply.github.com>
2023-03-29 22:05:45 +0200

    feat: creating dropdown menu in the header

--- src/components/green-grocery/login/WelcomeOnPage.js
+++ src/components/green-grocery/login/WelcomeOnPage.js
@@ -1,12 +1,15 @@
+import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
 import { authActions } from '../../../store/slices/auth';
+import classes from './WelcomeOnPage.module.css';
 
 const WelcomeOnPage = () => {
   const dispatch = useDispatch();
   const userNickname = useSelector((state) => state.auth.loggedUserEmail.split('@').shift());
   const history = useHistory();
+  const [showDropdown, setShowDropdown] = useState(false);
 
   const goToOrdersPage = () => {
     history.push('/green-grocery/orders');
@@ -17,14 +20,20 @@ const WelcomeOnPage = () => {
     history.push('/green-grocery');
   };
 
+  const toggleDropdown = () => {
+    setShowDropdown((prevState) => !prevState);
+  };
+
+  const cssClassWelcomeDropdown = `dropdown-menu ${showDropdown ? 'show' : ''} ${classes.dropdownPosition}`;
+
   return (
     <div className='navbar-brand d-flex dropdown' role='button'>
-      <a className='dropdown-toggle text-decoration-none' role='button' data-bs-toggle='dropdown' aria-expanded='false'>
+      <a className='dropdown-toggle text-decoration-none' role='button' onClick={toggleDropdown}>
         <span>
           Welcome <span>{userNickname}</span> !
         </span>
       </a>
-      <ul className='dropdown-menu'>
+      <ul className={cssClassWelcomeDropdown}>
         <li>
           <a className='dropdown-item' onClick={goToOrdersPage}>
             My orders
